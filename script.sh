f3325fa5ae9c268b47a468d86e0090d537d982e6
farrah-madkour <farrah.madkour@ibm.com>
2023-03-24 11:50:11 -0700

    update playbook to create project and job template

---
+++ script.sh
@@ -0,0 +1,7 @@
+ansible-playbook --extra-vars="@vars.yaml" playbook.yml
+docker build -t ansible-execution-env . --platform linux/x86_64
+
+
+https://github.com/ansible-supercloud/ansible-execution-env.git
+
+https://github.com/ansible-supercloud/sample-capability.git
